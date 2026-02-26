02041cfa3c6dee3b3e5812ab59d7b191040bf643
Shanisanal <shani.sanal@trenser.com>
2026-02-26 13:07:07 +0000

    Added project header and copyright notice to gzip.h

--- Include/gzip/gzip.h
+++ Include/gzip/gzip.h
@@ -1,7 +1,35 @@
+//****************************** File_Utility_Task  *************************************
+// Copyright (c) 2026 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : gzip.h
+// Summary  : Header file for gzip compression module
+// Note     : Provides function declarations for gzip compression utilities 
+// Author   : Shani 
+// Date     : Feb 26, 2026 
+//
+//*****************************************************************************
+
 #ifndef GZIP_H 
 #define GZIP_H
 
+//******************************* Include Files ******************************* 
+
+//******************************* Global Types ******************************** 
+ 
+//***************************** Global Constants ****************************** 
+ 
+//***************************** Global Variables ****************************** 
+ 
+//**************************** Forward Declarations *************************** 
 
 void gzip_file(const char *Input, const char *Output);
 
+//***************************** Class Declaration ***************************** 
+ 
+//*********************** Inline Method Implementations *********************** 
+
 #endif
+
+// EOF 

41cd86927b5b2f4009becf6e9c608014b3a8f97b
Shanisanal <shani.sanal@trenser.com>
2026-02-27 06:25:16 +0000

    Created hexdump.h file which will contain function declarations for hexdump utilities

---
+++ Include/hexdump/hexdump.h
@@ -0,0 +1,32 @@
+//****************************** File_Utility_Task  *************************************
+// Copyright (c) 2026 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : hexdump.h
+// Summary  : Header file for hexdump utility module
+// Note     : Provides function declarations for hexdump utilities 
+// Author   : Shani 
+// Date     : Feb 27, 2026 
+//
+//*****************************************************************************
+#ifndef HEXDUMP_H
+#define HEXDUMP_H
+
+//******************************* Include Files ******************************* 
+
+//******************************* Global Types ******************************** 
+ 
+//***************************** Global Constants ****************************** 
+ 
+//***************************** Global Variables ****************************** 
+ 
+//**************************** Forward Declarations *************************** 
+
+//***************************** Class Declaration ***************************** 
+ 
+//*********************** Inline Method Implementations *********************** 
+
+#endif
+
+// EOF 

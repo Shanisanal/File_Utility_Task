e41786dd4b30edbd4b518c69092b636a9b28439e
Shanisanal <shani.sanal@trenser.com>
2026-02-27 08:10:39 +0000

    Implemented hexdump_file() in hexdump.c

--- Source/hexdump/hexdump.c
+++ Source/hexdump/hexdump.c
@@ -12,10 +12,83 @@
 //***************************************************************************** 
  
 //******************************* Include Files ******************************* 
+#include <stdint.h>
+#include <stdio.h>
+#include <ctype.h>
 
 //******************************* Local Types ********************************* 
  
 //***************************** Local Constants ******************************* 
+#define HEXDUMP_BYTES_PER_LINE 16
+
+//***************************** Local Variables ******************************* 
+ 
+//****************************** Local Functions ******************************
+
+//****************************** hexdump_file ******************************
+// Purpose : Generates a hexadecimal dump of the input file and writes the formatted output (hex values + ASCII equivalents) to
+//           the specified output file.
+// Inputs  : pInput  - path to the input file 
+//           pOutput - path to the output file 
+// Outputs : Creates a text file containing the hexdump at the given output path.
+// Return  : None
+// Notes   : None
+//*****************************************************************************
+void hexdump_file(const char *pInput, const char *pOutput) 
+{
+    FILE *fin = fopen(pInput, "rb");
+
+    FILE *fout = fopen(pOutput, "w");
+
+    if (!fin || !fout) 
+    {
+        perror("File open failed");
+        if (fin) 
+        {
+            fclose(fin);
+        }
+        if (fout) 
+        {
+            fclose(fout);
+        } 
+        return;
+    }
+
+    uint8_t ucbuffer[HEXDUMP_BYTES_PER_LINE];    
+    size_t bytesRead;
+    size_t offset ;
+
+    while ((bytesRead = fread(ucbuffer, 1, sizeof(ucbuffer), fin)) > 0) 
+    {
+        fprintf(fout, "%08lx  ", offset);
+
+        // Write hex values
+        for (size_t i = 0; i < 16; i++) 
+        {
+            if (i < bytesRead)
+            {
+               fprintf(fout, "%02x ", ucbuffer[i]);
+            }
+            else
+            {
+                fprintf(fout, "   ");
+            }
+        }
+
+        // Write ASCII equivalents
+        fprintf(fout, " |");
+        for (size_t i = 0; i < bytesRead; i++) 
+        {
+            fprintf(fout, "%c", isprint(ucbuffer[i]) ? ucbuffer[i] : '.');
+        }
+        fprintf(fout, "|\n");
+
+        offset += bytesRead;
+    }
+
+    fclose(fin);
+    fclose(fout);
+}
 
 
 //EOF

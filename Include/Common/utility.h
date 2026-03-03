3c92e645f45be37b628240e3011134812e176a3c
Shanisanal <shani.sanal@trenser.com>
2026-03-03 10:56:02 +0000

    Initialzed header file with headers

---
+++ Include/Common/utility.h
@@ -0,0 +1,45 @@
+//****************************** File_Utility_Task  *************************************
+// Copyright (c) 2026 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : utility.h
+// Summary  : Header file for utility functions
+// Note     : Provides function declarations for utility functions 
+// Author   : Shani 
+// Date     : Feb 27, 2026 
+//
+//*****************************************************************************
+#ifndef UTILITY_H
+#define UTILITY_H
+
+//******************************* Include Files ******************************* 
+#include <stdint.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+//******************************* Global Types ******************************** 
+ 
+//***************************** Global Constants ****************************** 
+#define FILE_MODE_READ_BINARY  "rb"
+#define FILE_MODE_WRITE_BINARY "wb"
+#define FILE_MODE_WRITE_TEXT   "w"
+//***************************** Global Variables ****************************** 
+ 
+ typedef struct 
+ { 
+    uint8_t*  pucArgumentType; 
+    uint8_t*  pucInputFileName; 
+    uint8_t*  pucOutputFileName; 
+} ARGUMENTS; 
+
+//**************************** Forward Declarations *************************** 
+bool ParseArguments(uint16_t unArgCount, uint8_t* pucArgv[], ARGUMENTS* pstArguments) ;
+bool RunUtility(ARGUMENTS* pstArguments) ;
+
+//*********************** Inline Method Implementations *********************** 
+
+#endif
+
+// EOF 

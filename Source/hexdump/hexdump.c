9b823e3cb7e581154f447e238508614e0187ca6e
Shanisanal <shani.sanal@trenser.com>
2026-03-06 04:26:47 +0000

    Error print correction

--- Source/hexdump/hexdump.c
+++ Source/hexdump/hexdump.c
@@ -44,7 +44,7 @@ bool HexdumpConvert(uint8_t* pucInput, uint8_t* pucOutput)
 
     if(pucInput == NULL || pucOutput == NULL) 
     {
-        fprintf(stderr, "Error: Input and output file paths NULL.\n");
+        fprintf(stderr, "Error: Input or output file paths NULL.\n");
         blSuccess = false;
     }
 
